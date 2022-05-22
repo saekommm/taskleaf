5eeafa9e60865f6fce02df80eb78608bed46b558
Saeko Matsuzaki <saekorian@gmail.com>
2022-05-22 10:55:57 +0900

    genbarails ch.4-3-3

--- db/migrate/20220521130609_change_tasks_name_limit30.rb
+++ db/migrate/20220521130609_change_tasks_name_limit30.rb
@@ -3,7 +3,7 @@ class ChangeTasksNameLimit30 < ActiveRecord::Migration[5.2]
     def up
       change_column :tasks, :name, :string, limit: 30
     end
-    def doqn
+    def down
       change_column :tasks, :name, :string
     end
   end
