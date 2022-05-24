0ad030a00deb70aba725a5912f966dad8429001f
Saeko Matsuzaki <saekorian@gmail.com>
2022-05-24 23:59:38 +0900

    genbarails ch.4-5-13 error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_072029) do
+ActiveRecord::Schema.define(version: 2022_05_24_134551) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
