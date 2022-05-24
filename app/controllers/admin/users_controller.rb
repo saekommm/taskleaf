75915ca2795ac0f9eff4c02c2f11d37c06d10e13
Saeko Matsuzaki <saekorian@gmail.com>
2022-05-24 22:43:04 +0900

    genbarails ch.4-5-11 first

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -20,7 +20,7 @@ class Admin::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
     end
